9abd472b1cca058d95e18fe9f7708b4600398a0e
mvkaioz <112037312+mvkaioz@users.noreply.github.com>
2023-02-04 01:39:54 -0800

    History Layout
    Got the history layout pretty much done. just need to recolor later on

--- HistoryPage.js
+++ HistoryPage.js
@@ -2,15 +2,41 @@
 
 // Code Initiated by Aiden Sabater
 
-// Code Initiated by Aiden Sabater
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,  Text, View, TouchableOpacity, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function HistoryPage() {
+export default function HistoryPage({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>History Page</Text>
+
+
+        <View style={styles.BigMainRectangle}>
+            <View style={styles.SmallMendRectangle}>
+                <Text style={styles.InitialMSG}>Hope you're doing great! Here is your Mend Report from this DATE</Text>
+            </View>
+            <View style={styles.UserFeedbackRectangle}>
+                <Text style={styles.FeedbackMSG}>USER INPUT</Text>
+            </View>
+
+        </View>
+
+
+
+
+
+
+             {/* Icon NavBar Buttons */}
+             <View style={styles.NavBarButtons}>
+             <Icon onPress={() => navigation.navigate('CalendarPage')} name="calendar-heart" size={70} color="black"/>
+             <Icon onPress={() => navigation.navigate('HistoryPage')} name="history" size={70} color="black"/>
+             <Icon onPress={() => navigation.navigate('Home')} name="home-group" size={70} color="black"/>
+             <Icon onPress={() => navigation.navigate('JournalPage')} name="comment-question" size={70} color="black"/>
+
+             {/* THIS IS JUST A PLACE HOLDER */}
+             <Icon onPress={() => navigation.navigate('VoiceMemo')} name="account-alert" size={70} color="black"/>
+            </View>
+
         </View>
     )
 }
@@ -18,8 +44,60 @@ export default function HistoryPage() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#20444c',
+      backgroundColor: '#e8e6d9',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    NavBarButtons: {
+        top: '10%',
+        alignContent: 'center',
+        flexDirection: 'row',
+        marginHorizontal:50,
+        marginTop:10,
+      },
+    BigMainRectangle: {
+        backgroundColor: 'black',
+        top:'3%',
+        height: 710,
+        width: 390,
+        borderRadius: 19,
+      },
+    SmallMendRectangle: {
+        backgroundColor: 'green',
+        alignContent: 'center',
+        left: '6%',
+        top: '4%',
+        height: 70,
+        width: 345,
+        borderRadius: 19,
+    },
+    InitialMSG: {
+        top: '13%',
+        left: '3%',
+        alignContent: 'center',
+        justifyContent: 'center',
+        fontSize: '16x',
+        fontWeight: '500',
+        color: 'black',
+        margin: 1, 
+    },    
+    UserFeedbackRectangle: {
+        backgroundColor: 'green',
+        alignContent: 'center',
+        left: '6%',
+        top: '8%',
+        height: 200,
+        width: 345,
+        borderRadius: 19,
+    },
+    FeedbackMSG: {
+        top: '13%',
+        left: '3%',
+        alignContent: 'center',
+        justifyContent: 'center',
+        fontSize: '16x',
+        fontWeight: '500',
+        color: 'black',
+        margin: 1, 
     },
   });
