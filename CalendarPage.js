5b899996f71ed6367565f0b59fe9df0c9d744534
CJ Apar <capar@ucsc.edu>
2023-02-03 22:26:50 -0800

    Installed calendar module + basic calendar to CalendarPage.

--- CalendarPage.js
+++ CalendarPage.js
@@ -3,6 +3,7 @@ import { StyleSheet,  Text, View, TouchableOpacity, Button } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Calendar } from 'react-native-calendars';
 
 import TextMemo from './TextMemo';
 import VoiceMemo from './VoiceMemo';
@@ -10,19 +11,19 @@ import VoiceMemo from './VoiceMemo';
 export default function CalendarPage({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Calendar Page</Text>
-            <Button
-                title="textButton"
-                onPress={() => navigation.navigate('TextMemo')}
-            >
-                test
-            </Button>
+            <Calendar style={styles.calendar}/>
             <Button 
                 title="voiceButton"
                 onPress={() => navigation.navigate('VoiceMemo')}
             >
                 test
             </Button>
+            <TouchableOpacity style={styles.textButton}
+                title="textButton"
+                onPress={() => navigation.navigate('TextMemo')}
+            >
+                <Text>Journal Entry</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -35,4 +36,15 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    calendar: {
+        borderRadius: 10,
+        width: 350,
+        height: 350,
+        top: -120,
+    },
+    textButton: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+    }
   });
