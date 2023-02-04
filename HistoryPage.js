8d1ed2fc71098eea2879327de93105af43c40261
mvkaioz <112037312+mvkaioz@users.noreply.github.com>
2023-02-03 23:49:08 -0800

    Place Holder for History Page
    Place Holder for History Page

--- HistoryPage.js
+++ HistoryPage.js
@@ -1,2 +1,25 @@
 // History Page will display previous journals from specific dates
+
 // Code Initiated by Aiden Sabater
+
+// Code Initiated by Aiden Sabater
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,  Text, View, TouchableOpacity, Button } from 'react-native';
+
+export default function HistoryPage() {
+    return (
+        <View style={styles.container}>
+            <Text>History Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#20444c',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
