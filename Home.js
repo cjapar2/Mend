6dc8554514c58f5808efccec325a58430fe32a57
CJ Apar <capar@ucsc.edu>
2023-02-04 02:08:31 -0800

    Pushing a combination of me and Aiden's commits. Conflicts with styling.

--- Home.js
+++ Home.js
@@ -52,45 +52,19 @@ export default function Home({navigation}) {
       </Modal>
         <StatusBar style="auto" />
         <Text style={styles.welcome}>Welcome User.</Text>
-<<<<<<< HEAD
-        <Text style={styles.MendWelcome}>Helping mend your mood one day at a time!</Text>
-
-         <View style={styles.BigRectangle}>
-            <Text style={styles.header}>How are we feeling today?</Text>
-
-            {/* Color Square Buttons */}
-             <View style={styles.moodButtons}>
-                <Icon onPress={() => saveMood('sad')} name="square" size={50} color="red"/>
-                <Icon onPress={() => saveMood('kinda sad')} name="square" size={50} color="orange" />
-                <Icon onPress={() => saveMood('neutral')} name="square" size={50} color="yellow"/>
-                <Icon onPress={() => saveMood('kinda happy')} name="square" size={50} color="lightgreen"/>
-                <Icon onPress={() => saveMood('happy')} name="square" size={50} color="green"/>
-                <Icon onPress={() => saveMood('sad')} name="square" size={50} color="green"/>
-            </View> 
-
-
-
-       </View>
-
-        {/* Icon NavBar Buttons */}
-        <View style={styles.NavBarButtons}>
-        <Icon onPress={() => navigation.navigate('CalendarPage')} name="calendar-heart" size={70} color="black"/>
-        <Icon onPress={() => navigation.navigate('HistoryPage')} name="history" size={70} color="black"/>
-        <Icon onPress={() => navigation.navigate('Home')} name="home-group" size={70} color="black"/>
-        <Icon onPress={() => navigation.navigate('JournalPage')} name="comment-question" size={70} color="black"/>
-
-        {/* THIS IS JUST A PLACE HOLDER */}
-        <Icon onPress={() => navigation.navigate('VoiceMemo')} name="account-alert" size={70} color="black"/>
-=======
         <Text style={styles.header}>How are we feeling today?</Text>
-        {/* Icon Buttons */}
-        <View style={styles.moodButtons}>
-          <Icon onPress={() => navigation.navigate('CalendarPage')} name="square" size={40} color="red"/>
-          <Icon onPress={() => {console.log('test')}} name="square" size={40} color="orange"/>
-          <Icon onPress={() => {console.log('test')}} name="square" size={40} color="yellow"/>
-          <Icon onPress={() => {console.log('test')}} name="square" size={40} color="lightgreen"/>
-          <Icon onPress={() => {console.log('test')}} name="square" size={40} color="green"/>
-          <Icon onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={40} />
+
+        <View style={styles.BigRectangle}>
+          <Text style={styles.header}>How are we feeling today?</Text>
+          {/* Icon Buttons */}
+          <View style={styles.moodButtons}>
+            <FAIcon onPress={() => saveMood('date', 'red', 'sad')} name="square" size={50} color="red"/>
+            <FAIcon onPress={() => saveMood('orange')} name="square" size={50} color="orange" />
+            <FAIcon onPress={() => saveMood('yellow')} name="square" size={50} color="yellow"/>
+            <FAIcon onPress={() => saveMood('lightgreen')} name="square" size={50} color="lightgreen"/>
+            <FAIcon onPress={() => saveMood('green')} name="square" size={50} color="green"/>
+            <FAIcon onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={40} />
+          </View>
         </View>
 
         {/* Icon NavBar Buttons */}
@@ -116,26 +90,10 @@ const styles = StyleSheet.create({
       height:'100%'
     },
     welcome: {
-<<<<<<< HEAD
-        bottom: '5%',
-        right: '14%',
-        fontSize: '35px',
-        fontWeight: '900',
-        color: 'black',
-    },
-    MendWelcome:{
-      bottom: '4%',
-      right: '4%',
-      fontSize: '15px',
-      fontStyle: 'italic',
-      fontWeight: '400',
-      color: 'black',
-=======
         bottom: '27%',
         right: '18%',
         fontSize: '30px',
         color: 'white',
->>>>>>> 32107c9ca000493069aef94450a3bf33faf623d6
     },
     header: {
         bottom: '20%',
@@ -147,22 +105,6 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         flexDirection: 'row',
     },
-<<<<<<< HEAD
-    NavBarButtons: {
-      top: '20%',
-      alignContent: 'center',
-      flexDirection: 'row',
-      marginHorizontal:50,
-    },
-    BigRectangle: {
-      backgroundColor: 'black',
-      justifyContent: 'center',
-      alignItems: 'center',
-      top:'6%',
-      height: 570,
-      width: 360,
-      borderRadius: 19,
-=======
     popUpHeader:{
       alignItems:'center',
       backgroundColor:'#FFFFFF',
@@ -205,7 +147,15 @@ const styles = StyleSheet.create({
     },
     newEmotionButton:{
       marginBottom:"20%"
->>>>>>> 32107c9ca000493069aef94450a3bf33faf623d6
-    }
+    },
+    BigRectangle: {
+      backgroundColor: 'cyan',
+      justifyContent: 'center',
+      alignItems: 'center',
+      top:'6%',
+      height: 570,
+      width: 360,
+      borderRadius: 19,
+    },
   });
   
