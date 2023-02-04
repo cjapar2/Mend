7d77c946fb924a7c1804b5e710c43a9760ab0c62
CJ Apar <capar@ucsc.edu>
2023-02-04 00:00:45 -0800

    Added local storage module and basic storage working.

--- CalendarPage.js
+++ CalendarPage.js
@@ -4,23 +4,49 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Calendar } from 'react-native-calendars';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import TextMemo from './TextMemo';
 import VoiceMemo from './VoiceMemo';
 
 export default function CalendarPage({navigation}) {
+
+    /* Function that loads mood to local storage */
+    /* TODO: Use this data to individually color calendar days */
+    const loadMood = async () => {
+        try {
+            // create date object
+            let d = new Date();
+            // format date DD-MM-YYYY
+            let dFormatted = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
+            console.log('current date', dFormatted);
+            let mood = await AsyncStorage.getItem(dFormatted);
+            // get test data
+            let test = await AsyncStorage.getItem('test')
+            console.log('test data:', test)
+            if (mood != null) {
+                console.log(mood);
+            }
+        } catch (err) {
+            alert(err);
+        }
+    }
     return (
-        <View style={styles.container}>
+        <View style={styles.container} theme={{textDayFontSize: 200}}>
             <Calendar style={styles.calendar}/>
-            <Button 
-                title="voiceButton"
-                onPress={() => navigation.navigate('VoiceMemo')}
-            >
-                test
-            </Button>
+            <Icon style={styles.voiceButton} name="microphone"
+                onPress={() => navigation.navigate('TextMemo')}
+                size={30}
+            />
             <TouchableOpacity style={styles.textButton}
                 title="textButton"
                 onPress={() => navigation.navigate('TextMemo')}
+            >
+                <Text>Journal Entry</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.textButton}
+                title="textButton"
+                onPress={() => loadMood()}
             >
                 <Text>Journal Entry</Text>
             </TouchableOpacity>
@@ -38,13 +64,19 @@ const styles = StyleSheet.create({
     },
     calendar: {
         borderRadius: 10,
-        width: 350,
+        width: 375,
         height: 350,
-        top: -120,
+        top: -90,
     },
     textButton: {
         backgroundColor: 'white',
         borderRadius: 10,
         padding: 20,
+    },
+    voiceButton: {
+        backgroundColor: 'white',
+        borderRadius: 500,
+        display: 'inline-block',
+        padding: 20
     }
   });
--- Home.js
+++ Home.js
@@ -1,8 +1,35 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,  Text, View, TouchableOpacity, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Home({navigation}) {
+  const [mood, setMood] = React.useState('')
+  
+  /* Function that saves mood to local storage */
+  const saveMood = async (e) => {
+    try {
+      const d = new Date();
+      let dFormatted = d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+      // set key with value e
+      await AsyncStorage.setItem(dFormatted, e);
+      // test data
+      await AsyncStorage.setItem('test', 'this shows up if key is test')
+      console.log('stored data', e);
+      // navigate to calendar page
+      navigation.navigate('CalendarPage');
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <View style={styles.container}>
         <StatusBar style="auto" />
@@ -10,14 +37,13 @@ export default function Home({navigation}) {
         <Text style={styles.header}>How are we feeling today?</Text>
         {/* Icon Buttons */}
         <View style={styles.moodButtons}>
-            <Icon onPress={() => navigation.navigate('CalendarPage')} name="square" size={50} color="red"/>
-            <Icon onPress={() => {console.log('test')}} name="square" size={50} color="orange" />
-            <Icon onPress={() => {console.log('test')}} name="square" size={50} color="yellow"/>
-            <Icon onPress={() => {console.log('test')}} name="square" size={50} color="lightgreen"/>
-            <Icon onPress={() => {console.log('test')}} name="square" size={50} color="green"/>
-            <Icon onPress={() => {console.log('test')}} name="square" size={50} color="green"/>
+            <Icon onPress={() => saveMood('sad')} name="square" size={50} color="red"/>
+            <Icon onPress={() => saveMood('kinda sad')} name="square" size={50} color="orange" />
+            <Icon onPress={() => saveMood('neutral')} name="square" size={50} color="yellow"/>
+            <Icon onPress={() => saveMood('kinda happy')} name="square" size={50} color="lightgreen"/>
+            <Icon onPress={() => saveMood('happy')} name="square" size={50} color="green"/>
+            <Icon onPress={() => saveMood('sad')} name="square" size={50} color="green"/>
         </View>
-
     </View>
   )
 }
@@ -25,7 +51,7 @@ export default function Home({navigation}) {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#20444c',
+      backgroundColor: '#e8e6d9',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -44,8 +70,6 @@ const styles = StyleSheet.create({
         bottom: '0%',
         alignContent: 'center',
         flexDirection: 'row',
-        
-
     },
   });
   
