8ff1bb181677fa29022dd0cb6cdb005914d8d642
jishli113 <j.li.113w@gmail.com>
2023-02-04 23:59:39 -0800

    home UI improvements

--- Home.js
+++ Home.js
@@ -77,19 +77,21 @@ export default function Home({navigation}) {
       </Modal>
       <StatusBar style="auto" />
 
-      {/* Icon Buttons */}
-      <View style={styles.moodButtons}>
-        {allEmotions.map((emotion)=>{
-          return (
-            <View key={emotion.color} style={styles.moodButtonView}>
-              <FAIcon style={styles.moodButton} onPress={()=>saveMood(emotion.color,emotion.emotion)} name="square" size={85} color={emotion.color}/>
-              <Text style={styles.moodSquareEmotionText}>{emotion.emotion}</Text>
-            </View>
-          )
-        })}
+        {/* Icon Buttons */}
+        <View style={styles.moodButtonsWrap}>
+          <View style={styles.moodButtons}>
+            {allEmotions.map((emotion)=>{
+              return (
+                <View key={emotion.color} style={styles.moodButtonView}>
+                  <FAIcon style={styles.moodButton} onPress={()=>saveMood(emotion.color,emotion.emotion)} name="square" size={85} color={emotion.color}/>
+                  <Text style={styles.moodSquareEmotionText}>{emotion.emotion}</Text>
+                </View>
+              )
+            })}
 
-        <FAIcon style={styles.moodButton}onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={85} />
-      </View>
+            <FAIcon style={styles.moodButton}onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={85} />
+          </View>
+        </View>
 
       {/* Icon NavBar Buttons */}
       <View style={styles.NavBarButtons}>
@@ -101,130 +103,142 @@ export default function Home({navigation}) {
   )
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:'100%'
-  },
-  moodButton:{
-    marginLeft:10,
-    marginRight:10,
-    marginTop:10,
-    marginBottom:6
-  },
-  welcome: {
-    position: 'relative',
-    bottom: '26%',
-    right: '24%',
-    fontSize: '34px',
-    fontWeight: '700',
-    color: 'white',
-    animationType:'fade-in',
-    
-  },
-  MendMissionStatement: {
-    bottom: '26%',
-    padding: 3,
-    right: '7%',
-    fontSize: '14 px',
-    fontStyle: 'italic',
-    fontWeight: '300',
-    color: 'white',
-    animationType:'fade-in'
-  },
-  NavBarButtons:{
-    flexDirection:'row',
-    position:'absolute',
-    bottom:"1%"
-  },
-  header: {
-    bottom: '23%',
-    alignContent: 'center',
-    justifyContent: 'center',
-    left: '2%',
-    fontStyle: 'italic',
-    fontWeight: '600',
-    fontSize: '25px',
-    color: 'white',
-  },
-  moodButtons: {
-      position: 'absolute',
-      bottom: '10%',
+    container: {
+      flex: 1,
+      backgroundColor: 'transparent',
+      alignItems: 'center',
+      justifyContent: 'center',
+      height:'100%'
+    },
+    moodButton:{
+      marginLeft:10,
+      marginRight:10,
+      marginTop:10,
+      marginBottom:6
+    },
+    welcome: {
+      position: 'relative',
+      bottom: '19%',
+      right: '24%',
+      fontSize: '34px',
+      fontWeight: '700',
+      color: 'white',
+      animationType:'fade-in',
+      
+    },
+    MendMissionStatement: {
+      bottom: '19%',
+      padding: 3,
+      right: '7%',
+      fontSize: '14 px',
+      fontStyle: 'italic',
+      fontWeight: '300',
+      color: 'white',
+      animationType:'fade-in'
+    },
+    NavBarButtons:{
+      flexDirection:'row',
+      position:'absolute',
+      bottom:"1%"
+    },
+    header: {
+      bottom: '13%',
       alignContent: 'center',
-      marginLeft:'5%',
-      marginRight:'5%',
+      justifyContent: 'center',
+      left: '2%',
+      fontStyle: 'italic',
+      fontWeight: '600',
+      fontSize: '25px',
+      color: 'white',
+      marginLeft:'3%',
+      marginRight:'3%'
+    },
+    moodButtons: {
+        width:'90%',
+        position: 'fixed',
+        bottom: '35%',
+        alignContent: 'center',
+        marginLeft:'auto',
+        marginRight:'auto',
+        flexDirection: 'row',
+        flexWrap:'wrap',
+    },
+    moodButtonsWrap:{
+      position:'relative',
+      height:'20%',
+      marginLeft:'18%',
+      width:'100%',
+      marginTop:'20%'
+    },
+    moreMoodButtons: {
+      justifyContent: 'center',
+      bottom: '20%',
       flexDirection: 'row',
-      flexWrap:'wrap',
-  },
-  moreMoodButtons: {
-    justifyContent: 'center',
-    bottom: '20%',
-    flexDirection: 'row',
-    marginTop: 25,
-  },
-  popUpHeader:{
-    alignItems:'center',
-    backgroundColor:'#FFFFFF',
-    width:'90%',
-    height:'75%',
-    marginTop:'auto',
-    marginBottom:'auto',
-    marginLeft:'auto',
-    marginRight:'auto'
-  },
-  popUpHeaderText:{
-    fontSize:'25px',
-    color:'#000000',
-    marginTop:'10%'
-  },
-  modalContainer:{
-    height:'100%'
-  },
-  popUpClose:{
-    marginLeft:'80%',
-    marginTop:'4%',
-  },
-  colorPicker:{
-    marginLeft:'auto',
-    marginRight:'auto',
-    bottom:'15%',
-    flex:1
-  },
-  colorChooseText:{
-    fontSize:'25px',
-    marginTop:'10%'
-  },
-  newEmotionText:{
-    marginTop:'15%',
-    borderWidth:'1',
-    color:'#000000',
-    width:250,
-    height:35,
-  },
-  newEmotionButton:{
-    marginBottom:"20%"
-  },
-  colorSelectorView:{
-    height:"40%",
-    marginBottom:"15%"
-  },
-  moodSquareEmotionText:{
-    fontSize:"14px",
-    color:'#000000',
-    marginLeft:'auto',
-    marginRight:'auto'
-  },
-  moodButtonView:{
-    flexDirection:"wrap"
-  },
-  GradientBG: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 1000
-  },
-});
+      marginTop: 25,
+    },
+    popUpHeader:{
+      alignItems:'center',
+      backgroundColor:'#FFFFFF',
+      width:'90%',
+      height:'75%',
+      marginTop:'auto',
+      marginBottom:'auto',
+      marginLeft:'auto',
+      marginRight:'auto'
+    },
+    popUpHeaderText:{
+      fontSize:'25px',
+      color:'#000000',
+      marginTop:'10%'
+    },
+    modalContainer:{
+      height:'100%'
+    },
+    popUpClose:{
+      marginLeft:'80%',
+      marginTop:'4%',
+    },
+    colorPicker:{
+      marginLeft:'auto',
+      marginRight:'auto',
+      bottom:'15%',
+      flex:1
+    },
+    colorChooseText:{
+      fontSize:'25px',
+      marginTop:'10%'
+    },
+    newEmotionText:{
+      marginTop:'15%',
+      borderWidth:'1',
+      color:'#000000',
+      width:250,
+      height:35,
+    },
+    newEmotionButton:{
+      marginBottom:"20%"
+    },
+    colorSelectorView:{
+      height:"40%",
+      marginBottom:"15%",
+      width:"90%"
+    },
+    moodSquareEmotionText:{
+      fontSize:"14px",
+      color:'#000000',
+      marginLeft:'auto',
+      marginRight:'auto'
+    },
+    moodButtonView:{
+      flexDirection:"wrap"
+    },
+    GradientBG: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      height: 1000
+    },
+
+  });
   
