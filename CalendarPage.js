644db128f94142c508865479b9a15b5e91d4d7a6
mvkaioz <112037312+mvkaioz@users.noreply.github.com>
2023-02-05 00:24:18 -0800

    UI Changes to CalendarPage
    Made major UI changes to calendar page

--- CalendarPage.js
+++ CalendarPage.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,  Text, View, TouchableOpacity, Modal } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Calendar } from 'react-native-calendars';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useEffect, useState} from 'react'
-import FAIcon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
-export default function CalendarPage({route}) {
+export default function CalendarPage({route, navigation}) {
     const [calendarColorData, setCalendarColorData] = useState()
     const [isLoading, setIsLoading] = useState(true)
     const [modalVisible, setModalVisible] = useState(false);
@@ -50,9 +50,14 @@ export default function CalendarPage({route}) {
     return (
         <View style={styles.container} theme={{textDayFontSize: 200}}>
 
+        <LinearGradient
+        // Background Linear Gradient
+        colors={['#FFA071', '#FFB480', '#FEC98F', '#FEDD9E','#FDF1AD']}
+        style={styles.GradientBG}/>
+
             <Modal style={{}} visible={modalVisible}>
                 <View style={styles.popUpHeader}>
-                    <FAIcon style={styles.popUpClose} size={20} name="close" onPress={()=>setModalVisible(false)}></FAIcon>
+                    <Icon style={styles.popUpClose} size={20} name="close" onPress={()=>setModalVisible(false)}></Icon>
                     <Text style={styles.popUpHeader}>Current Day: {modalDate["day"]}</Text>
                 </View>
             </Modal>
@@ -64,24 +69,37 @@ export default function CalendarPage({route}) {
                     setModalDate(e);
                 }}
             />
-            <Icon style={styles.voiceButton} name="microphone"
+            {/* <Icon style={styles.voiceButton} name="microphone"
                 onPress={() => navigation.navigate('JournalPage')}
                 size={30}
-            />
+            /> */}
 
-            <TouchableOpacity style={styles.textButton}
+            <TouchableOpacity style={styles.JournalButton}
                 title="textButton"
                 onPress={() => navigation.navigate('JournalPage')}
             >
 
-                <Text>Journal Entry</Text>
+                <Text style={styles.JournalButtonText}>Journal Entry</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.textButton}
+            {/* <TouchableOpacity style={styles.textButton}
                 title="textButton"
                 onPress={() => loadMood()}
             >
                 <Text>Journal Entry</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
+
+            <View style={styles.MicButton}>
+             <Icon onPress={() => navigation.navigate('Home')} name="microphone-plus" size={100} color="black"/>
+            </View> 
+
+            {/* Icon NavBar Buttons */}
+            <View style={styles.NavBarButtons}>
+             <Icon onPress={() => navigation.navigate('CalendarPage')} name="calendar-heart" size={70} color="#FF8547"/>
+             <Icon onPress={() => navigation.navigate('Home')} name="home-group" size={70} color="#FF8547"/>
+             <Icon onPress={() => navigation.navigate('HistoryPage')} name="history" size={70} color="#FF8547"/>
+            </View>
+
+
 
         </View>
     )
@@ -100,26 +118,58 @@ const styles = StyleSheet.create({
         width: 375,
         height: 350,
         top: -90,
+        alignContent: 'center',
+        top: -25,
     },
-    textButton: {
+    JournalButton: {
         backgroundColor: 'white',
+        alignContent: 'center',
+        justifyContent: 'center',
+        top: '12%',
+        right: '18%',
+        height: 90,
+        width: 130,
         borderRadius: 10,
         padding: 20,
-    },
-    voiceButton: {
-        backgroundColor: 'white',
-        borderRadius: 500,
-        display: 'inline-block',
-        padding: 20
     },
     popUpHeader:{
       alignItems:'center',
       backgroundColor:'#FFFFFF',
       width:'90%',
-      height:'75%',
+      height:'80%',
       marginTop:'auto',
       marginBottom:'auto',
       marginLeft:'auto',
       marginRight:'auto'
     },
+    GradientBG: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: 1000
+      },
+      NavBarButtons: {
+        top: '35%',
+        alignContent: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+      },
+      MicButton: {
+        top: '5%',
+        left: '10%',
+        alignContent: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+      },
+      JournalButtonText: {
+        fontSize: 20, 
+        fontWeight: '800',
+        alignContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
+        padding: 1
+
+      }
   });
