607ef65d471c6b6ffc1272233a78256b001d63bc
CJ <cjapar2@gmail.com>
2023-02-04 23:37:20 -0800

    Fixed indentations in CalendarPage.js.

--- Home.js
+++ Home.js
@@ -43,17 +43,18 @@ export default function Home({navigation}) {
   return (
     <View style={styles.container}>
 
-<LinearGradient
+      <LinearGradient
         // Background Linear Gradient
         colors={['#FFA071', '#FFB480', '#FEC98F', '#FEDD9E','#FDF1AD']}
         start={{ x: 0.7, y: 0 }}
         style={styles.GradientBG}
       />
-        <Text style={styles.welcome}>Welcome!</Text>
-        <Text style={styles.MendMissionStatement}>Helping mend your day one color at a a time</Text>
+    
+      <Text style={styles.welcome}>Welcome!</Text>
+      <Text style={styles.MendMissionStatement}>Helping mend your day one color at a a time</Text>
         
 
-<Text style={styles.header}>What color describes your mood today?</Text>
+      <Text style={styles.header}>What color describes your mood today?</Text>
 
       <Modal style={styles.modalContainer}visible={isPoppedUp} animationType="slide" transparent={true} onRequestClose={()=>{setIsPoppedUp(false)}}>
         <View style={styles.popUpHeader}>
@@ -68,162 +69,162 @@ export default function Home({navigation}) {
             <ColorPicker
         onColorChange={(color)=>{setNewEmotionColor(color)}}
         style={styles.colorPicker} thumbSize={15} sliderHidden={true} gapSize={-40}
-      />
+        />
+
         </View>
-        <Button style={styles.newEmotionButton}title="Create" onPress={()=>handlePopupClose()}></Button>
+          <Button style={styles.newEmotionButton}title="Create" onPress={()=>handlePopupClose()}></Button>
         </View>
       </Modal>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
 
-        {/* Icon Buttons */}
-        <View style={styles.moodButtons}>
-          {allEmotions.map((emotion)=>{
-            return (
-              <View key={emotion.color} style={styles.moodButtonView}>
-                <FAIcon style={styles.moodButton} onPress={()=>saveMood(emotion.color,emotion.emotion)} name="square" size={85} color={emotion.color}/>
-                <Text style={styles.moodSquareEmotionText}>{emotion.emotion}</Text>
-              </View>
-            )
-          })}
+      {/* Icon Buttons */}
+      <View style={styles.moodButtons}>
+        {allEmotions.map((emotion)=>{
+          return (
+            <View key={emotion.color} style={styles.moodButtonView}>
+              <FAIcon style={styles.moodButton} onPress={()=>saveMood(emotion.color,emotion.emotion)} name="square" size={85} color={emotion.color}/>
+              <Text style={styles.moodSquareEmotionText}>{emotion.emotion}</Text>
+            </View>
+          )
+        })}
 
-          <FAIcon style={styles.moodButton}onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={85} />
-        </View>
+        <FAIcon style={styles.moodButton}onPress={() => {setIsPoppedUp(true)}} name="plus-square" size={85} />
+      </View>
 
-        {/* Icon NavBar Buttons */}
-        <View style={styles.NavBarButtons}>
-        <MCIcon onPress={() => navigation.navigate('CalendarPage')} name="calendar-heart" size={70} color="#FF8547"/>
-        <MCIcon onPress={() => navigation.navigate('Home')} name="home-group" size={70} color="#FF8547"/>
-        <MCIcon onPress={() => navigation.navigate('HistoryPage')} name="history" size={70} color="#FF8547"/>
-        </View>
+      {/* Icon NavBar Buttons */}
+      <View style={styles.NavBarButtons}>
+      <MCIcon onPress={() => navigation.navigate('CalendarPage')} name="calendar-heart" size={70} color="#FF8547"/>
+      <MCIcon onPress={() => navigation.navigate('Home')} name="home-group" size={70} color="#FF8547"/>
+      <MCIcon onPress={() => navigation.navigate('HistoryPage')} name="history" size={70} color="#FF8547"/>
+      </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'transparent',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height:'100%'
-    },
-    moodButton:{
-      marginLeft:10,
-      marginRight:10,
-      marginTop:10,
-      marginBottom:6
-    },
-    welcome: {
-      position: 'relative',
-      bottom: '26%',
-      right: '24%',
-      fontSize: '34px',
-      fontWeight: '700',
-      color: 'white',
-      animationType:'fade-in',
-      
-    },
-    MendMissionStatement: {
-      bottom: '26%',
-      padding: 3,
-      right: '7%',
-      fontSize: '14 px',
-      fontStyle: 'italic',
-      fontWeight: '300',
-      color: 'white',
-      animationType:'fade-in'
-    },
-    NavBarButtons:{
-      flexDirection:'row',
-      position:'absolute',
-      bottom:"1%"
-    },
-    header: {
-      bottom: '23%',
-      alignContent: 'center',
-      justifyContent: 'center',
-      left: '2%',
-      fontStyle: 'italic',
-      fontWeight: '600',
-      fontSize: '25px',
-      color: 'white',
-    },
-    moodButtons: {
-        position: 'absolute',
-        bottom: '10%',
-        alignContent: 'center',
-        marginLeft:'5%',
-        marginRight:'5%',
-        flexDirection: 'row',
-        flexWrap:'wrap',
-    },
-    moreMoodButtons: {
-      justifyContent: 'center',
-      bottom: '20%',
-      flexDirection: 'row',
-      marginTop: 25,
-    },
-    popUpHeader:{
-      alignItems:'center',
-      backgroundColor:'#FFFFFF',
-      width:'90%',
-      height:'75%',
-      marginTop:'auto',
-      marginBottom:'auto',
-      marginLeft:'auto',
-      marginRight:'auto'
-    },
-    popUpHeaderText:{
-      fontSize:'25px',
-      color:'#000000',
-      marginTop:'10%'
-    },
-    modalContainer:{
-      height:'100%'
-    },
-    popUpClose:{
-      marginLeft:'80%',
-      marginTop:'4%',
-    },
-    colorPicker:{
-      marginLeft:'auto',
-      marginRight:'auto',
-      bottom:'15%',
-      flex:1
-    },
-    colorChooseText:{
-      fontSize:'25px',
-      marginTop:'10%'
-    },
-    newEmotionText:{
-      marginTop:'15%',
-      borderWidth:'1',
-      color:'#000000',
-      width:250,
-      height:35,
-    },
-    newEmotionButton:{
-      marginBottom:"20%"
-    },
-    colorSelectorView:{
-      height:"40%",
-      marginBottom:"15%"
-    },
-    moodSquareEmotionText:{
-      fontSize:"14px",
-      color:'#000000',
-      marginLeft:'auto',
-      marginRight:'auto'
-    },
-    moodButtonView:{
-      flexDirection:"wrap"
-    },
-    GradientBG: {
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height:'100%'
+  },
+  moodButton:{
+    marginLeft:10,
+    marginRight:10,
+    marginTop:10,
+    marginBottom:6
+  },
+  welcome: {
+    position: 'relative',
+    bottom: '26%',
+    right: '24%',
+    fontSize: '34px',
+    fontWeight: '700',
+    color: 'white',
+    animationType:'fade-in',
+    
+  },
+  MendMissionStatement: {
+    bottom: '26%',
+    padding: 3,
+    right: '7%',
+    fontSize: '14 px',
+    fontStyle: 'italic',
+    fontWeight: '300',
+    color: 'white',
+    animationType:'fade-in'
+  },
+  NavBarButtons:{
+    flexDirection:'row',
+    position:'absolute',
+    bottom:"1%"
+  },
+  header: {
+    bottom: '23%',
+    alignContent: 'center',
+    justifyContent: 'center',
+    left: '2%',
+    fontStyle: 'italic',
+    fontWeight: '600',
+    fontSize: '25px',
+    color: 'white',
+  },
+  moodButtons: {
       position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      height: 1000
-    },
-
-  });
+      bottom: '10%',
+      alignContent: 'center',
+      marginLeft:'5%',
+      marginRight:'5%',
+      flexDirection: 'row',
+      flexWrap:'wrap',
+  },
+  moreMoodButtons: {
+    justifyContent: 'center',
+    bottom: '20%',
+    flexDirection: 'row',
+    marginTop: 25,
+  },
+  popUpHeader:{
+    alignItems:'center',
+    backgroundColor:'#FFFFFF',
+    width:'90%',
+    height:'75%',
+    marginTop:'auto',
+    marginBottom:'auto',
+    marginLeft:'auto',
+    marginRight:'auto'
+  },
+  popUpHeaderText:{
+    fontSize:'25px',
+    color:'#000000',
+    marginTop:'10%'
+  },
+  modalContainer:{
+    height:'100%'
+  },
+  popUpClose:{
+    marginLeft:'80%',
+    marginTop:'4%',
+  },
+  colorPicker:{
+    marginLeft:'auto',
+    marginRight:'auto',
+    bottom:'15%',
+    flex:1
+  },
+  colorChooseText:{
+    fontSize:'25px',
+    marginTop:'10%'
+  },
+  newEmotionText:{
+    marginTop:'15%',
+    borderWidth:'1',
+    color:'#000000',
+    width:250,
+    height:35,
+  },
+  newEmotionButton:{
+    marginBottom:"20%"
+  },
+  colorSelectorView:{
+    height:"40%",
+    marginBottom:"15%"
+  },
+  moodSquareEmotionText:{
+    fontSize:"14px",
+    color:'#000000',
+    marginLeft:'auto',
+    marginRight:'auto'
+  },
+  moodButtonView:{
+    flexDirection:"wrap"
+  },
+  GradientBG: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 1000
+  },
+});
   
